b5417629dad5cff410043c76e3a5d60897b8afd1
thenishantgiri <nishantkrgiri@gmail.com>
2023-10-26 14:43:23 +0530

    linked zsh runcoms dotfiles

---
+++ .zsh/aliases.zsh
@@ -0,0 +1,24 @@
+# Alias
+# ---
+#
+alias k="kubectl"
+alias h="helm"
+alias tf="terraform"
+alias a="ansible"
+alias ap="ansible-playbook"
+alias dt="datree"
+
+# mac OS shortcuts
+alias code="open -a 'Visual Studio Code'"
+
+
+
+# ALIAS COMMANDS
+alias cls="clear"
+alias ls="exa --icons --group-directories-first"
+alias ll="exa --icons --group-directories-first -l"
+alias la="exa --icons --group-directories-first -l -all"
+alias lt="exa --icons --group-directories-first -l -all --tree"
+alias g="goto"
+alias grep='grep --color'
+
